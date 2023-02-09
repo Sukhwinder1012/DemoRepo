2a2687d85e39317dc81e3b59efd2702dc82ec7c4
Sukhwinder1012 <sukhwinder101291.ss@gmail.com>
2023-02-09 20:53:48 +0530

    My first commit of this selenium code

---
+++ src/test/java/com/qa/tests/LoginTests.java
@@ -0,0 +1,19 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTests {
+
+	@Test
+	public void test1() {
+		System.out.println("987");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
